2f85f9e84964287cb566ebfb59cb38d53e5ec522
Danil Grigorev <danil.grigorev@suse.com>
2024-06-06 09:27:10 +0200

    Add docstring generation, schemars derive and ObjectReference inheritance (#56)
    Signed-off-by: Danil-Grigorev <danil.grigorev@suse.com>

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,7 +1,7 @@
 pub mod capi_cluster;
 pub mod capi_clusterclass;
-pub mod fleet_cluster;
-pub mod fleet_clustergroup;
 pub mod fleet_addon_config;
+pub mod fleet_cluster;
 #[cfg(feature = "agent-initiated")]
 pub mod fleet_cluster_registration_token;
+pub mod fleet_clustergroup;
